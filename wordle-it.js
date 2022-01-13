c30c7867705425bfb8421ed38327b441074fd261
Ramiro Rela <ramiro.rela@gmail.com>
2022-01-13 10:20:07 -0300

    Tinchisize success message and word not in list message

--- wordle-it.js
+++ wordle-it.js
@@ -1044,7 +1044,7 @@ this.wordle = this.wordle || {}, this.wordle.bundle = function(e) {
   var Qa = "IN_PROGRESS",
       Za = "WIN",
       es = "FAIL",
-      as = ["Genio!!!", "Magnifico", "Notevole", "Ottimo", "Non male", "Daje!"],
+      as = ["Tinchazooo!!!", "Tercer tiemo ea! ea!", "Notevole", "Ottimo", "Non male", "Daje!"],
       ss = function(e) {
           n(t, e);
           var a = h(t);
@@ -1071,7 +1071,7 @@ this.wordle = this.wordle || {}, this.wordle.bundle = function(e) {
                   if (5 === this.tileIndex && !(this.rowIndex >= 6)) {
                       var e, a = this.$board.querySelectorAll("game-row")[this.rowIndex],
                           s = this.boardState[this.rowIndex];
-                      if (e = s, !La.includes(e) && !Aa.includes(e)) return a.setAttribute("invalid", ""), void this.addToast("Non nella lista di parole");
+                      if (e = s, !La.includes(e) && !Aa.includes(e)) return a.setAttribute("invalid", ""), void this.addToast("Un tincho no entendería eso");
                       if (this.hardMode) {
                           var t = function(e, a, s) {
                                   if (!e || !a || !s) return {
